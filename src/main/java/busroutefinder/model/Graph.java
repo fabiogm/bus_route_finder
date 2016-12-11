bcc8f902c40ef3b0036d34a7f7b29891c3dc74f4
fabiogm <fmagalhaes@gmail.com>
2016-12-11 21:03:12 -0200

    Small refactorings and implement DFS.

--- src/main/java/busroutefinder/model/Graph.java
+++ src/main/java/busroutefinder/model/Graph.java
@@ -18,8 +18,8 @@ public class Graph {
     }
 
     public void addEdge(Edge v) {
-        Integer first = v.getFirst();
-        Integer second = v.getSecond();
+        Integer first = v.getStart();
+        Integer second = v.getEnd();
 
         if (!adjacencyList.containsKey(first)) {
             adjacencyList.put(first, new ArrayList<>());
@@ -36,19 +36,15 @@ public class Graph {
     public boolean areConnected(Integer start, Integer end) {
         Predicate<Integer> predicate = (e) -> e == end;
 
-        return doBFS(start, predicate);
+        return breadthFirstSearch(start, predicate);
     }
 
-    public boolean breadthFirstSearch(Integer source) {
-        return doBFS(source, x -> true);
-    }
-
-    public boolean breadthFirstSearch(Integer source, Predicate<Integer> shouldStop) {
-        return doBFS(source, shouldStop);
+    public boolean pathTo(Integer start, Integer end, Stack<Edge> path) {
+        return depthFirstSearch(start, path, e -> e == end);
     }
 
     /**
-     * doBFS
+     * breadthFirstSearch
      *
      * Perform breadth-first search on graph, starting from node source.
      *
@@ -56,7 +52,7 @@ public class Graph {
      * @param shouldStop - predicate for early stoppage
      * @return true if search stopped before end of graph, otherwise return false.
      */
-    public boolean doBFS(Integer source, Predicate<Integer> shouldStop) {
+    public boolean breadthFirstSearch(Integer source, Predicate<Integer> shouldStop) {
         Queue<Integer> q = new LinkedList<>();
         HashSet<Integer> visitedVertices = new HashSet<>();
         boolean shouldContinue = !shouldStop.test(source);
@@ -80,6 +76,52 @@ public class Graph {
         }
 
         return !shouldContinue;
+    }
+
+    /**
+     * depthFirstSearch
+     *
+     * Perform depth-first search on graph, starting from node source and keeping track of path until
+     * shouldStop criteria is true.
+     *
+     * @param source - starting node of search
+     * @param path - stack keeping track of path (set of edges) from source to last node reached when
+     *             search is stopped.
+     * @param shouldStop - predicate for early stoppage
+     * @return true if search stopped before end of graph, otherwise return false.
+     */
+    public boolean depthFirstSearch(Integer source, Stack<Edge> path, Predicate<Integer> stoppagePredicate) {
+        HashSet<Integer> visitedVertices = new HashSet<>();
+
+        return depthFirstSearch(source, visitedVertices, path, stoppagePredicate);
+    }
+
+    public boolean depthFirstSearch(Integer source, HashSet<Integer> visitedVertices, Stack<Edge> path,
+                                             Predicate<Integer> stoppagePredicate) {
+        visitedVertices.add(source);
+
+        if (stoppagePredicate.test(source)) {
+            return true;
+        }
+
+        List<Integer> adjVertices = getNeighbourhood(source);
+
+        for (Integer w : adjVertices) {
+            if (!visitedVertices.contains(w)) {
+
+                path.push(new Edge(source, w));
+
+                boolean res = depthFirstSearch(w, visitedVertices, path, stoppagePredicate);
+
+                if (res) {
+                    return true;
+                }
+
+                path.pop();
+            }
+        }
+
+        return false;
     }
 
     private List<Integer> getNeighbourhood(Integer vertex) {
--- src/test/java/busroutefinder/model/GraphTest.java
+++ src/test/java/busroutefinder/model/GraphTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
+import java.util.Stack;
 import java.util.function.Predicate;
 
 import static com.google.common.collect.Lists.newArrayList;
@@ -24,6 +25,8 @@ public class GraphTest {
 
     private Graph sut;
 
+    private Stack<String> stack;
+
     @Before
     public void setUp() {
         sut = new Graph();
@@ -31,7 +34,12 @@ public class GraphTest {
         sut.addEdge(new Edge(1, 2));
         sut.addEdge(new Edge(1, 3));
         sut.addEdge(new Edge(1, 4));
+        sut.addEdge(new Edge(3, 4));
         sut.addEdge(new Edge(3, 5));
+        sut.addEdge(new Edge(5, 6));
+        sut.addEdge(new Edge(5, 7));
+
+        stack = new Stack<>();
     }
 
     @Test
@@ -44,6 +52,9 @@ public class GraphTest {
         verify(stoppagePredicate, times(1)).test(2);
         verify(stoppagePredicate, times(1)).test(3);
         verify(stoppagePredicate, times(1)).test(4);
+        verify(stoppagePredicate, times(1)).test(5);
+        verify(stoppagePredicate, times(1)).test(6);
+        verify(stoppagePredicate, times(1)).test(7);
     }
 
     @Test
@@ -53,13 +64,12 @@ public class GraphTest {
 
         sut.breadthFirstSearch(1, stoppagePredicate);
 
-        assertThat(captor.getAllValues(), equalTo(newArrayList(1, 2, 3, 4, 5)));
+        assertThat(captor.getAllValues(), equalTo(newArrayList(1, 2, 3, 4, 5, 6, 7)));
     }
 
     @Test
     public void shouldStopEarlyBreadthFirstSearch() {
         ArrayList<Integer> actualInvocations = new ArrayList<>();
-
         when(stoppagePredicate.test(anyInt())).thenAnswer(invocation -> {
             int firstMockArg = (Integer) invocation.getArguments()[0];
             actualInvocations.add(firstMockArg);
@@ -67,6 +77,7 @@ public class GraphTest {
         });
 
         assertTrue(sut.breadthFirstSearch(1, stoppagePredicate));
+
         assertThat(actualInvocations, equalTo(newArrayList(1, 2, 3)));
     }
 
@@ -78,5 +89,43 @@ public class GraphTest {
     @Test
     public void shouldNotFindConnectedNodes() {
         assertTrue(sut.areConnected(2, 5));
+    }
+
+    @Test
+    public void shouldSearchDepthFirst() {
+        ArgumentCaptor<Integer> captor = ArgumentCaptor.forClass(Integer.class);
+        doReturn(false).when(stoppagePredicate).test(captor.capture());
+        Stack<Edge> path = new Stack<>();
+        ArrayList<Integer> expectedCalls = newArrayList(1, 2, 3, 4, 5, 6, 7);
+
+        sut.depthFirstSearch(1, path, stoppagePredicate);
+
+        assertThat(captor.getAllValues(), equalTo(expectedCalls));
+    }
+
+    @Test
+    public void shouldFindPathSearchingDepthFirst() {
+        Stack<Edge> actualPath = new Stack<>();
+        Stack<Edge> expectedPath = new Stack<>();
+        expectedPath.push(new Edge(1, 3));
+        expectedPath.push(new Edge(3, 5));
+        expectedPath.push(new Edge(5, 6));
+
+        sut.depthFirstSearch(1, actualPath, e -> e == 6);
+
+        assertThat(actualPath, equalTo(expectedPath));
+    }
+
+    @Test
+    public void shouldFindPathBetweenTwoNodes() {
+        Stack<Edge> actualPath = new Stack<>();
+        Stack<Edge> expectedPath = new Stack<>();
+        expectedPath.push(new Edge(1, 3));
+        expectedPath.push(new Edge(3, 5));
+        expectedPath.push(new Edge(5, 7));
+
+        sut.pathTo(1, 7, actualPath);
+
+        assertThat(actualPath, equalTo(expectedPath));
     }
 }
