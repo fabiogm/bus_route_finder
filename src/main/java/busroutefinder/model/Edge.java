b3625a125ff2febc748a6cfa53f661a93e42c125
fabiogm <fmagalhaes@gmail.com>
2016-12-11 19:41:12 -0200

    Declutter Edge type.

--- src/main/java/busroutefinder/model/Edge.java
+++ src/main/java/busroutefinder/model/Edge.java
@@ -1,20 +1,15 @@
 package busroutefinder.model;
 
+import lombok.Getter;
+
+@Getter
 public class Edge {
 
-    private final Integer first;
-    private final Integer second;
+    private final Integer start;
+    private final Integer end;
 
-    public Edge(Integer first, Integer second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    public Integer getFirst() {
-        return first;
-    }
-
-    public Integer getSecond() {
-        return second;
+    public Edge(Integer start, Integer end) {
+        this.start = start;
+        this.end = end;
     }
 }
