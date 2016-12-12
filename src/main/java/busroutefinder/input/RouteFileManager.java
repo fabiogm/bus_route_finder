7f451647bf319075c0110618c05f2018ba9a5539
fabiogm <fmagalhaes@gmail.com>
2016-12-11 22:45:40 -0200

    Should be a Spring component.

--- src/main/java/busroutefinder/input/RouteFileManager.java
+++ src/main/java/busroutefinder/input/RouteFileManager.java
@@ -2,11 +2,13 @@ package busroutefinder.input;
 
 import busroutefinder.configuration.ConfigurationManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+@Component
 public class RouteFileManager {
 
     private BufferedReader routeFileBuffer;
