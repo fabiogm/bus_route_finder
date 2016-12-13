441d05d96f90718bdad671d4117b346a3b37b1ad
fabiogm <fmagalhaes@gmail.com>
2016-12-13 00:43:42 -0200

    Utility for creating set of nodes from list when searching for route.

--- src/main/java/busroutefinder/model/Edge.java
+++ src/main/java/busroutefinder/model/Edge.java
@@ -3,6 +3,9 @@ package busroutefinder.model;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @EqualsAndHashCode
 public class Edge {
@@ -13,6 +16,17 @@ public class Edge {
     public Edge(Integer start, Integer end) {
         this.start = start;
         this.end = end;
+    }
+
+    public static List<Edge> edgesFromVertices(List<Integer> vertices) {
+        int pathLength = vertices.size();
+        List<Edge> edges = new ArrayList<>();
+
+        for (int i=0; i < pathLength - 1; i++) {
+            edges.add(new Edge(vertices.get(i), vertices.get(i+1)));
+        }
+
+        return edges;
     }
 
     public String toString() {
